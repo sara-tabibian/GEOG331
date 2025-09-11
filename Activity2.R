efc4304cd46878097db1f99eab7e6b1f1d9efcf8
sara-tabibian <stabibian@colgate.edu>
2025-09-11 11:12:20 -0400

    homework 2 changes - in class 9/11

---
+++ Activity2.R
@@ -0,0 +1,37 @@
+#homework 2
+
+#make a vector of tree heights in meters
+
+heights <-c(30,41,20,22)
+#convert to cm
+
+heights_cm <-heights*100
+heights_cm
+
+#look at the first tree height
+heights[1]
+
+#look at the 2nd and 3rd tree heights
+heights[2:3]
+
+#get more info on the matrix function
+help(matrix)
+
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat <- matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+Mat
+
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+Mat.bycol
+
+#subset the matrix to look at row 1, column2
+Mat.bycol[1,2]
+
+#look at all values in row 1
+Mat.bycol[1,]
+
+#look at all values in column 2
+Mat.bycol[,2]
