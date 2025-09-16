1703ac1316d64d8eb8030bef9397fae4dba8999a
sara-tabibian <stabibian@colgate.edu>
2025-09-16 14:35:19 -0400

    got to question 3

--- Activity2.R
+++ Activity2.R
@@ -60,7 +60,51 @@ datW$year <-as.numeric(format(datW$dateF,"%Y"))
 datW$year
 datW$dateF
 
+#find out all unique site names
+unique(datW$NAME)
 
+#look at the mean maximum temperature for Aberdeen
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
 
+#look at the mean maximum temperature for Aberdeen
+#with na.rm argument set to true to ignore NA
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
 
+#calculate the average daily temperature
+#This temperature will be halfway between the minimum and maximum temperature
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+datW$TAVE
 
+#get the mean across all sites
+#the by function is a list of one or more variables to index over.
+#FUN indicates the function we want to use
+#if you want to specify any function specific arguments use a comma and add them after the function
+#here we want to use the na.rm arguments specific to 
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
+averageTemp
+
+#change the automatic output of column names to be more meaningful
+#note that MAAT is a common abbreviation for Mean Annual Air Temperature
+colnames(averageTemp) <- c("NAME","MAAT")
+averageTemp
+
+#change the automatic output of column names to be more meaningful
+#note that MAAT is a common abbreviation for Mean Annual Air Temperature
+colnames(averageTemp) <- c("NAME","MAAT")
+averageTemp
+
+#convert level to number for factor data type
+#you will have to reference the level output or look at the row of data to see the character designation.
+datW$siteN <- as.numeric(datW$NAME)
+
+#make a histogram for the first site in our levels
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+hist(datW$TAVE[datW$siteN == 1],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="grey50",
+     border="white")
