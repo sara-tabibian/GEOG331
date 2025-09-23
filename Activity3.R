afad5ad26e6e6843fab513f18d84a0ea66ad72c8
sara-tabibian <stabibian@colgate.edu>
2025-09-23 11:15:33 -0400

    CORRECT github push - 9/23 worked up to "Correcting missing data" section

---
+++ Activity3.R
@@ -0,0 +1,68 @@
+#testing your code#
+
+rm(list=ls())
+
+library(lubridate)
+#use install.packages to install lubridate
+install.packages(c("lubridate"))
+#package INSTALLED
+
+
+#create a function. The names of the arguments for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
+assert <- function(statement,err.message){
+  #if evaluates if a statement is true or false for a single item
+  if(statement == FALSE){
+    print(err.message)
+  }
+  
+}
+
+#check how the statement works
+#evaluate a false statement
+assert(1 == 2, "error: unequal values")
+
+#evaluate a true statement
+assert(2 == 2, "error: unequal values")
+#set up assert to check if two vectors are the same length
+a <- c(1,2,3,4)
+b <- c(8,4,5)
+assert(length(a) == length(b), "error: unequal length")
+
+####getting sensor info#####
+
+#read in the data file
+#skip the first 3 rows since there is additional column info
+#specify the the NA is designated differently
+datW <- read.csv("Z:\\stabibian\\github\\data\\bewkes\\bewkes_weather.csv",
+                 #skip=3 skips the first three rows in the data set; na.strings=c("#N/A) treats all missing values as NA
+                 na.strings=c("#N/A"), skip=3, header=FALSE) #header=FALSE tells it not to treat the first row as the column names; header=TRUE makes first row the column names
+#preview data
+print(datW[1,])
+
+#get sensor info from file
+# this data table will contain all relevant units
+sensorInfo <-   read.csv("Z:\\stabibian\\github\\data\\bewkes\\bewkes_weather.csv",
+                         na.strings=c("#N/A"), nrows=2)
+
+print(sensorInfo)
+
+#get column names from sensorInfo table
+# and set weather station colnames  to be the same
+colnames(datW) <- colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day (DD) of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calculations
+datW[1,]
+
+###CHECKING MISSING DATA###
