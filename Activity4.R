7194cf2685035e8678c53cfd1fbdea09e4851288
sara-tabibian <stabibian@colgate.edu>
2025-10-17 14:13:19 -0400

    Completed Activity 4; answer to the question is at the bottom of the code

--- Activity4.R
+++ Activity4.R
@@ -3,7 +3,9 @@
 head(iris)
 #load in some tidyverse packages
 install.packages("tidyverse")
+install.packages("ggpot2")
 library(tidyverse)
+library(ggplot2)
 #dply::filter(...) - use when conflict
 #####################################
 ##### Part 1: for loops         #####
@@ -17,22 +19,24 @@ library(tidyverse)
 #2. iris  petal length x width
 #3. iris sepal length x petal length
 
-list <- list() #creating empty list
+#subset from flower dataset
+versicolor <- iris[iris$Species == "versicolor",]
 
-#create vector to run through
+#create empty list
+reg <- list()
 
+#create vector of formulas
+var <- list(iris$Sepal.Length ~ iris$Sepal.Width, 
+                iris$Petal.Length ~ iris$Petal.Width,
+                iris$Sepal.Width ~ iris$Petal.Length) 
+
+#create for loop
 for (i in 1:3){
-  list[[i]] <- lm(iris$Sepal.Length ~ iris$Sepal.Width, 
-                  iris$Petal.Length ~ iris$Petal.Width,
-                  iris$Sepal.Width ~ iris$Petal.Length)
+  reg[[i]] <- summary(lm(var[[i]], data = versicolor))
 }
 
- print(list[i])
+reg
 
-#plot the residuals
-#lot(list[[i]], summary(fit)$residuals, pch = 19,
-    #xlab = "Length", ylab = "Width")
-#bline(h=0)
 
 # hint: consider using a list, and also new vectors for regression variables
 
@@ -47,6 +51,11 @@ for (i in 1:3){
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
+#create new data frame called new_iris
+#left_join matches values from y to x; keeps all rows from iris data set and adds "height"
+new_iris <- iris %>% left_join(height, by = "Species") #joins data set by matching "species" column
+
+
 
 
 #####################################
@@ -56,17 +65,37 @@ height <- data.frame(Species = c("virginica","setosa","versicolor"),
 #look at base R scatter plot
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
+
 #3a. now make the same plot in ggplot
 
+ggplot(iris, aes(x = Sepal.Length, #common aesthetic values (aes)
+                 y = Sepal.Width)) + geom_point() #geom_point used to represent data points
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
 
+#same code structure as 3a but add theme_minimal()
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + 
+  geom_point() +
+  theme_minimal() #customizes theme to minimal; default them is grey background
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species, #maps the species color to aes
+                 size = Petal.Length)) + #correlates petal length with dot size; maps variable from data to size of geom  
+  geom_point() +
+  theme_minimal() +
+  theme(panel.grid = element_blank()) + #removes major and minor grid lines from plot
+  labs(title = "Iris Sepal Measurements", #adds chart title
+       x = "Sepal Length (cm)", #adds axis titles
+       y = "Sepal Width (cm)")
 
 #####################################
 ##### Question: how did         #####
 ##### arguments differ between  #####
 ##### plot and ggplot?          #####
 #####################################		
+
+##In plot, everything is specified as parameters in one line of code
+##ggplot uses various layers with data and aesthetics (aes) then geom_point() adds the points
+##each layer has its specified function: aes() - aesthetics, geom() - plot type, labs() - labels, theme() - appearance 
