48782c5624a28168db4b7b86b25409fbe6ca2c48
sara-tabibian <stabibian@colgate.edu>
2025-11-02 20:00:36 -0500

    completed

--- Activity5.R
+++ Activity5.R
@@ -166,4 +166,75 @@ legend("topright", c("mean","1 standard deviation"), #legend items
 
 #QUESTION 5#
 
+year2017 <- subset(datD, year == 2017) #filter for 2017
+
+par(mai=c(1,1,1,1)) #margins
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,180),#adjust ylim
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.9, 0.2, 0.2,.3), #color that is semi-transparent
+        border=NA#no border
+) 
+lines(year2017$doy, year2017$discharge, col="orange", lwd=2)
+axis(1, at=seq(15, 345, by=30), #tick intervals
+     lab=month.abb) #tick labels
+axis(2, seq(0,180, by=20), #adjust y axis
+     seq(0,180, by=20),
+     las = 2)#show ticks at 90 degree angle
+legend("topright", c("mean","1 standard deviation", "2017 average"), #legend items
+       lwd=c(2,NA,2),#lines
+       col=c("black",rgb(0.9, 0.2, 0.2,.3), "orange"),#colors
+       pch=c(NA,15),#symbols
+       bty="n")#no legend border
+
+#QUESTION 7#
+
+#QUESTION 8#
+
+#making box plots and violin plots
+
+library(ggplot2)
+#specify year as a factor
+datD$yearPlot <- as.factor(datD$year)
+#make a boxplot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+  geom_boxplot()
+
+#make a violin plot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+  geom_violin()
+
+#QUESTION 9#
+
+#assigning DOY ranges to seasons
+datD$season <- with(datD, ifelse(doy %in% 1:59 | doy %in% 335:365, "Winter", 
+                                 ifelse(doy %in% 60:151, "Spring",
+                                        ifelse(doy %in% 152:243, "Summer", "Fall"))))
+
+#filtering data for each year
+
+dat2016 <- subset(datD, year == 2016)
+dat2017 <- subset(datD, year == 2017)
+
+#plot for 2016
+
+ggplot(dat2016, aes(x = season, y = discharge)) + 
+  geom_violin(fill = "blue", color = "black") +
+  labs(title = "discharge by season (2016)", x = "season", y = "Discharge ft^3/sec") +
+  theme_minimal()
+
+#plot for 2017
+
+ggplot(dat2017, aes(x = season, y = discharge)) + 
+  geom_violin(fill = "green", color = "black") +
+  labs(title = "discharge by season (2017)", x = "season", y = "Discharge ft^3/sec") +
+  theme_minimal()
 
