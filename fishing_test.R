16eb289ad17501ca88308524e22602e97770ec72
sara-tabibian <stabibian@colgate.edu>
2025-11-11 11:14:24 -0500

    in class

--- fishing_test.R
+++ fishing_test.R
@@ -1,5 +1,4 @@
-#readxl package loaded
-library(readxl)
+
 
 datW <- read.csv("Z:\\stabibian\\github\\finalProject\\projectData\\fleet-monthly-csvs-10-v3-2014\\fleet-monthly-csvs-10-v3-2014-01-01.csv")
 
