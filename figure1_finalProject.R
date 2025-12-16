1570d7d9aeb01539c543f83ecd7a62963db9dbb5
sara-tabibian <stabibian@colgate.edu>
2025-12-16 18:03:48 -0500

    final commit

--- figure1_finalProject.R
+++ figure1_finalProject.R
@@ -18,7 +18,7 @@ library(readxl)
 library(purrr)
 library(stringr)
 
-
+#create hotspot map based on data from one month (jan 2014) for testing purposes
 datW <- datW %>%
   mutate(
     date = as.Date(date),
@@ -40,7 +40,7 @@ ggplot(pacific_datW, aes(x = cell_ll_lon, y = cell_ll_lat)) + geom_bin2d(bins = 
     x = "longitude", y = "latitude"
   ) + 
   theme_minimal()
-
+#low key don't hotspot include in final analysis
 
 #load all datasets
 setwd("Z:\\stabibian\\github\\finalProject\\projectData\\fleet-monthly\\extracted_monthly")
@@ -50,9 +50,6 @@ xlsx_files <- list.files(
   recursive = TRUE,
   full.names = TRUE
 )
-
-length(xlsx_files)
-head(xlsx_files)
 
 fleet_data <- map_df(
   xlsx_files,
@@ -67,7 +64,7 @@ fleet_data <- map_df(
 
 fleet_data
 
-#prep dataset 
+#prep dataset for analysis
 fleet_data <- fleet_data %>%
   mutate(
     date = as.Date(date),
@@ -78,7 +75,7 @@ fleet_data <- fleet_data %>%
   )
 
 
-#proxies for suspicion ##
+#proxies for suspicion #
 df_tuna <- fleet_data %>% filter(geartype %in% tunaGear)
 
 df_tuna <- df_tuna %>%
@@ -92,6 +89,7 @@ cell_stats <- df_tuna %>%
   summarise(mean_hours = mean(fishing_hours),
             sd_hours = sd(fishing_hours))
 
+#join df_tuna with cell_stats
 df_tuna <- df_tuna %>%
   left_join(cell_stats, by = c("cell_ll_lat", "cell_ll_lon")) %>%
   mutate(intense_fishing = ifelse(fishing_hours > mean_hours + 2*sd_hours, 1, 0))
@@ -107,6 +105,7 @@ monthly_sus <- df_tuna %>%
   group_by(month) %>%
   summarise(sus_hours = sum(sus_score, na.rm = TRUE)) ##
 
+#create plot for sus_score
 ggplot(monthly_sus, aes(x=month, y=sus_hours)) + 
   geom_line(color= "red", linewidth=1.1) +
   theme_minimal() +
@@ -121,7 +120,7 @@ fao_raw <- read_csv("Z:\\stabibian\\github\\finalProject\\projectData\\FAO_fish_
                     skip = 3, col_names = TRUE)
 
 
-
+#prep dataset
 fao_tuna <- fao_raw %>%
  rename(
    date_raw = "Date",
@@ -130,21 +129,18 @@ fao_tuna <- fao_raw %>%
 ) %>%
 
 
-select(date_raw, tuna_price) ##
+select(date_raw, tuna_price)
 
-#fao_tuna %>%
-  #distinct(date_raw) %>%
-  #slice(1:20)
 
 #parse FAO dates
 fao_tuna <- fao_tuna %>%
   mutate(
     date_clean = date_raw %>%
       str_trim() %>%
-      str_replace_all("[^A-Za-z0-9-]", ""),
+      str_replace_all("[^A-Za-z0-9-]", ""), #lowercase
     date = parse_date_time(
       date_clean,
-      orders = c("y-b", "yb")
+      orders = c("y-b", "yb") #fix date format
     ) %>% as.Date(),
     tuna_price = as.numeric(tuna_price)
   ) %>%
@@ -159,7 +155,7 @@ monthly_tuna <- fao_tuna %>%
     .groups = "drop"
   )
 
-#join
+#join to make a combined graph on same scale
 combined <- monthly_sus %>%
   inner_join(monthly_tuna, by = "month")
 
@@ -198,21 +194,12 @@ ggplot(combined_scale, aes(x = month, y = value, color = series)) +
 
 #run analysis on tuna prices vs sus fishing
 
-#correlation
-cor_test <- cor.test(combined$tuna_price, combined$sus_hours, method = "pearson")
-cor_test
-
 #linear regression
 lm <- lm(sus_hours ~ tuna_price, data = combined)
 summary(lm)
 
-#cross correlation up to 6 months lag
-ccf(combined$tuna_price, combined$sus_hours, lag.max = 6, 
-    main="Price-Suspicious Cross-Correlation")
-
-
 #create price_regime column for simplicity
-#create 80th percentile threshold
+#create 80th percentile threshold (80% over the mean of 2014-2024)
 p80 <- quantile(combined$tuna_price, 0.80, na.rm = TRUE)
 
 #create high/low label for each month
@@ -223,10 +210,10 @@ combined <- combined %>%
 
 table(combined$price_regime)
 
-#get p-value
-t.test(sus_hours ~ price_regime, data = combined)
+#welch t test
+t.test(sus_hours ~ price_regime, data = combined) #p-value=0.31
 
-#visual comparison plot
+#visual comparison box plot
 ggplot(combined, aes(x = price_regime,y = sus_hours, fill = price_regime)) +
   geom_boxplot(alpha = 0.7) +
   labs(
@@ -238,7 +225,7 @@ ggplot(combined, aes(x = price_regime,y = sus_hours, fill = price_regime)) +
   theme_minimal()
 
 
-
+#plot of just tuna price index
 ggplot(fao_tuna, aes(x= date, y = tuna_price)) +
   geom_line(color = "blue", linewidth = 1) +
   labs (
@@ -250,9 +237,97 @@ ggplot(fao_tuna, aes(x= date, y = tuna_price)) +
 
 
 
+#lag graph
+combined <- combined %>%
+  arrange(month) %>%
+  mutate(
+    price_high = ifelse(price_regime == "High Price", 1, 0)
+  )
+
+
+#check
+
+table(combined$price_regime, combined$price_high)
+
+lags <- 0:6 #0-6 months lag
+lag_df <- map_dfr(lags, \(L){
+  tmp <- combined %>%
+    mutate(price_high_lag = dplyr::lag(price_high, n = L))
+  
+  tibble(
+    lag = L,
+    cor = cor(tmp$sus_hours, tmp$price_high_lag, use = "complete.obs")
+  )
+})
+                  
+
+lag_df
+
+#plot lag graph
+ggplot(lag_df, aes(x = lag, y = cor)) +
+  geom_col(fill = "steelblue") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(
+    title = "Relationship Lag Between High Price Months and Suspicious Fishing",
+    x = "lag in months (price leads to suspicious activity)",
+    y = "correlation (r)"
+  ) + 
+  
+  theme_minimal(base_size = 13)
+
+#create lag graph for 1 month lag (most significant at 1 month)
+
+lag_chosen <- 1
+
+lag_plot_df <- combined %>%
+  arrange(month) %>%
+  mutate(
+    price_high = ifelse(price_regime == "High Price", 1, 0),
+    price_high_lag = dplyr::lag(price_high, n = lag_chosen)
+  ) 
+
+
+#fit the regression model
+fit <- lm(sus_hours ~ price_high_lag, data = lag_plot_df)
+summary_fit <- summary(fit)
+summary(fit)
+
+slope <- coef(fit)["price_high_lag"]
+r2 <- summary_fit$r.squared
+p_value <- summary_fit$coefficients["price_high_lag", "Pr(>|t|)"]
+
+#round
+slope_txt <- round(slope, 2)
+r2_txt <- round(r2, 2)
+p_txt <- signif(p_value, 3)
+p_label <- if (p_value < 0.001) "< 0.001" else as.character(p_txt)
+
+lag_plot <- ggplot(lag_plot_df, aes(x = price_high_lag, y = sus_hours)) +
+  geom_jitter(width = 0.05, height = 0, alpha = 0.7) +
+  geom_smooth(method = "lm", se = TRUE, color = "magenta", linewidth = 1) +
+  scale_x_continuous(breaks = c(0,1),
+                     labels = c("Low/Normal Price", "High Price")) +
+labs(
+  title = paste("Suspicious Fishing vs High Price Indicator (Lag =", lag_chosen, "month)" ),
+  subtitle = paste0("linear model: sus_hours ~ price_high_lag | ",
+  "slope = ", slope_txt, #put results directly onto graph
+  ", R^2 = ", r2_txt,
+  ", p = ", p_label
+),
+  x = "high price indicator (0 = low price month, 1 = high price month",
+  y = "suspicious score (sus_hours)"
+) +
+  theme_minimal(base_size = 13)
+  
+print(lag_plot)
+
+
+
 ###################################################################################################################
-#create hotspot map#
+#create hotspot map# 
 ##################################################################################################################
+
+##note*** don't include in final analysis
 
 #define pacific island region
 
@@ -326,97 +401,6 @@ ggplot() +
   ) +
   
   theme_minimal(base_size = 13)
-  
-
-#make better lag graph
-
-combined <- combined %>%
-  arrange(month) %>%
-  mutate(
-    price_high = ifelse(price_regime == "High Price", 1, 0)
-  )
-
-
-#check
-
-table(combined$price_regime, combined$price_high)
-
-lags <- 0:6
-lag_df <- map_dfr(lags, \(L){
-  tmp <- combined %>%
-    mutate(price_high_lag = dplyr::lag(price_high, n = L))
-  
-  tibble(
-    lag = L,
-    cor = cor(tmp$sus_hours, tmp$price_high_lag, use = "complete.obs")
-  )
-})
-                  
-
-lag_df
-
-#plot lag graph
-
-ggplot(lag_df, aes(x = lag, y = cor)) +
-  geom_col(fill = "steelblue") +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  labs(
-    title = "Relationship Lag Between High Price Months and Suspicious Fishing",
-    x = "lag in months (price leads to suspicious activity)",
-    y = "correlation (r)"
-  ) + 
-  
-  theme_minimal(base_size = 13)
-
-#create lag graph for 1 month lag
-
-lag_chosen <- 1
-
-lag_plot_df <- combined %>%
-  arrange(month) %>%
-  mutate(
-    price_high = ifelse(price_regime == "High Price", 1, 0),
-    price_high_lag = dplyr::lag(price_high, n = lag_chosen)
-  ) 
-
-
-#fit the regression model
-fit <- lm(sus_hours ~ price_high_lag, data = lag_plot_df)
-summary_fit <- summary(fit)
-summary(fit)
-
-slope <- coef(fit)["price_high_lag"]
-r2 <- summary_fit$r.squared
-p_value <- summary_fit$coefficients["price_high_lag", "Pr(>|t|)"]
-
-#round
-slope_txt <- round(slope, 2)
-r2_txt <- round(r2, 2)
-p_txt <- signif(p_value, 3)
-p_label <- if (p_value < 0.001) "< 0.001" else as.character(p_txt)
-
-lag_plot <- ggplot(lag_plot_df, aes(x = price_high_lag, y = sus_hours)) +
-  geom_jitter(width = 0.05, height = 0, alpha = 0.7) +
-  geom_smooth(method = "lm", se = TRUE, color = "magenta", linewidth = 1) +
-  scale_x_continuous(breaks = c(0,1),
-                     labels = c("Low/Normal Price", "High Price")) +
-labs(
-  title = paste("Suspicious Fishing vs High Price Indicator (Lag =", lag_chosen, "month)" ),
-  subtitle = paste0("linear model: sus_hours ~ price_high_lag | ",
-  "slope = ", slope_txt,
-  ", R^2 = ", r2_txt,
-  ", p = ", p_label
-),
-  x = "high price indicator (0 = low price month, 1 = high price month",
-  y = "suspicious score (sus_hours)"
-) +
-  theme_minimal(base_size = 13)
-  
-print(lag_plot)
-
-
-
-  
   
 
 
